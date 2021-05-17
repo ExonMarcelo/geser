f485ded21efe313e2372e5b92855e5f6c2aa4244
Exon Marcelo <exon.marcelo95@gmail.com>
2021-05-17 10:51:57 -0500

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/exonmarcelo/geser/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.exonmarcelo.geser.controller.MainView", {});
+});
